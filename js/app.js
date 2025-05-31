125a7baa33f79e3f957fc424df6ebacfc60b3fe7
Andrew556556 <144379643+Andrew556556@users.noreply.github.com>
2025-05-31 13:51:34 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -47,7 +47,7 @@ buttons.forEach(button => {
             speed: parseInt(speedRange.value)
         };
 
-        fetch('http://44.204.133.44:5000/api/devices', {
+        fetch('http://3.222.116.52:5000/api/devices', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(payload)
